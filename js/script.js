49a8cbd0fdfafe9ffa2a047e28d8a174e9c14592
brookeshayt <btbo89btbo@gmail.com>
2024-11-26 10:46:43 -0700

    edit 4

--- js/script.js
+++ js/script.js
@@ -3,36 +3,30 @@ console.log('Come work out with Henry the Hamster!');
 $("#button-1").on("click", function () {
     console.log('Dwayne "The Rock" Johnson, is that you?');
     $("#button-1").addClass("button-active");
-    $("#image").addClass("muscles");
-    // $("#image").html("<img src=\"images/Henry.png\">");
+    $(".image").html("<img src='../images/Henry.png'>");
 });
 
 $("#button-2").on("click", function () {
     console.log('Its about drive, its about power, we stay hungry, we devour');
     $("#button-2").addClass("button-active");
-    document.getElementById("image").style.backgroundImage = "url('../images/Henry2.png')";
-    // $("#image").css("background-image", "url(../images/Henry2.png);");
-    if (onclick == "#button-1", "#button-3", "#button-4", "#button-5") {
-        $("click").hide();
-    }
+    $(".image").html("<img src='../images/Henry2.png'>");
+    $("#button-1", "#button-3", "#button-4", "#button-5").hide();
 });
 
-if ($("#button-3").on("click", function () {
+$("#button-3").on("click", function () {
     console.log('Henry takes out his frustration in a demure way... with a punching bag');
     $("#button-3").addClass("button-active");
-    $("#image").html("<img src='../images/Henry3.png'>");
-}) == true) {
-    $("#button-2").hide("button-active");
-}
+    $(".image").html("<img src='../images/Henry3.png'>");
+});
 
 $("#button-4").on("click", function () {
     console.log('Weeeeeeeeeee');
     $("#button-4").addClass("button-active");
-    $("#image").html("<img src='../images/Henry4.png'>");
+    $(".image").html("<img src='../images/Henry4.png'>");
 });
 
 $("#button-5").on("click", function () {
     console.log('Ah yes, the classic hamster wheel. But Henry is no ordinary hamster');
     $("#button-5").addClass("button-active");
-    $("#image").html("<img src='../images/Henry5.png'>");
+    $(".image").html("<img src='../images/Henry5.png'>");
 });
